ae047f1d3c3aa803d39d094de143c5ca43506852
Keijiro Takahashi <keijiro@gmail.com>
2026-07-02 22:21:33 +0900

    Add custom layouts (video, image-right override)

---
+++ layouts/image-right.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  image: {
+    type: String,
+  },
+  class: {
+    type: String,
+  },
+  backgroundSize: {
+    type: String,
+    default: '85%',
+  },
+})
+
+function resolveAssetUrl(url: string) {
+  if (url.startsWith('/'))
+    return import.meta.env.BASE_URL + url.slice(1)
+  return url
+}
+
+const style = computed(() => ({
+  backgroundImage: props.image ? `url("${resolveAssetUrl(props.image)}")` : undefined,
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center',
+  backgroundSize: props.backgroundSize,
+}))
+</script>
+
+<template>
+  <div class="grid grid-cols-2 w-full h-full auto-rows-fr">
+    <div class="slidev-layout default" :class="props.class">
+      <slot />
+    </div>
+    <div class="w-full h-full" :style="style" />
+  </div>
+</template>
